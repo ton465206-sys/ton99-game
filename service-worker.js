571aac87c0402da7abc778087c70a0d43816541c
ton465206-sys <ton465206@gmail.com>
2026-07-02 09:54:00 +0700

    Refresh Play Store cache service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,17 @@
-const CACHE_NAME='jiksor-up-wild-v2';
-const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))));
+﻿const CACHE_NAME = 'jiksor-up-playstore-v3';
+const FILES = ['./','./index.html','./manifest.webmanifest','./icon.svg','./privacy.html','./parent-guide.html','./game-easy/','./game-easy/index.html','./game-upgrade/','./game-upgrade/index.html'];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES).catch(() => undefined)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => undefined);
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
